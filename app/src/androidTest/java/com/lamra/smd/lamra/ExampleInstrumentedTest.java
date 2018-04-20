d6cf5c5be8d7b3dd125933f0bca5600f422c75f4
LalainAhmad <lalainahmad96@gmail.com>
2018-04-20 13:53:18 +0500

    Random2

--- app/src/androidTest/java/com/lamra/smd/lamra/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/lamra/smd/lamra/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.lamra.smd.la2mra", appContext.getPackageName());
+        assertEquals("com.lamra.smd.lamra", appContext.getPackageName());
     }
 }
