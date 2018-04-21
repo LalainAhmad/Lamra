1d78d740166c2a74dc923f395604306dc4e6d4b2
LalainAhmad <lalainahmad96@gmail.com>
2018-04-22 04:12:33 +0500

    1fragment

--- app/src/main/java/com/lamra/smd/lamra/LoginActivity.java
+++ app/src/main/java/com/lamra/smd/lamra/LoginActivity.java
@@ -3,6 +3,7 @@ package com.lamra.smd.lamra;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -41,6 +43,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
+
+    protected void login(View v) {
+        Intent i = new Intent(this, NewsFeed.class);
+        startActivity(i);
+    }
+
+//    public void login(View view)
+//    {
+//
+//
+////        Log.i("Appinfo",String.valueOf(mEmailView.getText()));
+////        Log.i("Appinfo",String.valueOf(mPasswordView.getText()));
+//    }
+
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
@@ -82,12 +98,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
